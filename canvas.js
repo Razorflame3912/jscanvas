5e6168e12505718b000446c3dd2288fc33e85c96
Mansour <melsharawy@stuy.edu>
2018-02-05 14:45:41 -0500

    classwork

--- canvas.js
+++ canvas.js
@@ -3,12 +3,18 @@ var ctx = canvas.getContext('2d');
 var clearbutton = document.getElementById("clear");
 var togglebutton = document.getElementById("toggle");
 var shape = 0;
-
+var oldX = 0;
+var oldY = 0;
+var started = 0;
 ctx.fillStyle="red";
+ctx.beginPath();
 
 
 var clear = function(e){
     ctx.clearRect(0,0,500,500);
+    oldX = 0;
+    oldY = 0;
+    started = 0;
 };
 
 var toggle = function(e){
@@ -23,13 +29,23 @@ var toggle = function(e){
 };
 
 var draw = function(e){
-    ctx.beginPath();
     if(shape == 0){
-	ctx.arc(e.clientX-this.offsetLeft,e.clientY-this.offsetTop,10,0,2 * Math.PI);
+	ctx.beginPath();
+	moveTo(e.offsetX,e.offsetY);
+	ctx.arc(e.offsetX,e.offsetY,10,0,2 * Math.PI);
+	if(started != 0){
+	    ctx.lineTo(oldX,oldY);
+	}
+	else{
+	    started = 1;
+	}
 	ctx.fill();
+	ctx.stroke();
+	oldX = e.offsetX;
+	oldY = e.offsetY;
     }
     else{
-	ctx.fillRect(e.clientX-this.offsetLeft,e.clientY-this.offsetTop,20,20);
+	ctx.fillRect(e.offsetX,e.offsetY,20,20);
     }
 };
 
